d0fe09205336bee73dac40a534430588b30ca100
juanroyofullstack <juanroyofullstack@gmail.com>
2022-02-04 10:59:18 +0100

    iconos textos imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -246,6 +246,19 @@ function enviarEmail(e) {
 
 			}
 		});
+		$("header a").on('click', function(event) {
+			if (this.hash !== "") {
+				event.preventDefault();
+				var hash = this.hash;
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top
+				  }, 800, function(){
+			
+					// Add hash (#) to URL when done scrolling (default click behavior)
+					window.location.hash = hash;
+				  });
+			}
+		});
 
 })(jQuery);
 
