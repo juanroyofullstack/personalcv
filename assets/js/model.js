210c8368c4f89f209baaad03be56e14dd1bf1f4b
juanroyofullstack <juanroyofullstack@gmail.com>
2022-02-12 11:39:38 +0100

    tet

---
+++ assets/js/model.js
@@ -0,0 +1,14 @@
+export const state = {
+    name: false,
+    email: false,
+    subject: false,
+    text: false,
+    captcha: false,
+    
+}
+export const language = {
+    language: "ES"
+}
+export const render = {
+    render: false
+}
---
+++ assets/js/view/contactView.js
@@ -0,0 +1,46 @@
+import View from './view.js';
+import language from '../../language.json';
+
+class contactView extends View {
+    
+    _parentElement = document.querySelector('form');
+    _name = document.querySelector('#name')
+    _errorMessage = 'We could not find that recipe. Please try another one!';
+    _message = '';
+    addHandlerRender(handler) {
+        ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+      }
+    async renderContact(data) {
+        const markup = this._generateMarkup(language[data]);
+
+
+        this._clear();
+        return this._parentElement.insertAdjacentHTML('afterbegin', markup)
+    }
+    _generateMarkup(data) {
+        return `
+        <h3>${data.contact.title}</h3>
+            <div class="row gtr-uniform">
+                <div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="${data.contact.name}" /></div>
+                <div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="${data.contact.email}" /></div>
+                <div class="col-12"><input type="text" name="subject" id="subject" placeholder="${data.contact.subject}" /></div>
+                <div class="col-12"><textarea name="message" id="message" placeholder="${data.contact.message}" rows="6"></textarea></div>
+                <div class="col-12">
+                    <ul class="actions">
+                        <li><input type="submit" class="primary" value="${data.contact.send}" /></li>
+                        <li><div class="spinner" style="visibility: hidden;"></div></li>
+                        <!--<li><input id="reset" type="reset" value="Reset Form" /></li>-->
+                    </ul>
+                </div>
+            </div>
+            
+
+        <div class="captcha">
+            <canvas id="canvas"></canvas>
+            <input id="code" name="code" />
+            <button id="valid">send</button>
+        </div>
+        `;
+      }  
+}
+export default new contactView();
